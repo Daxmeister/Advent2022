3928cd710fab22b1b67f284d553a6ca0382faa52
alakazam <daxmsn@kfdd.mine.nu>
2022-12-04 01:15:49 +0100

    Added Comments.

--- main.py
+++ main.py
@@ -25,10 +25,14 @@ file.close()
 # Dag 1 del 2
 ################################################################################################################
 
+# Skapar en klass som håller koll på de tre högsta värden
 class ELf_list():
+
+    # Skapas med tre värden, kommer alltid att ha tre värden.
     def __init__(self):
         self.list = [0, 0, 0]
 
+    # Funktion som hittar det minsta talet. OBS att minsta talet måste vara mindre än 1 miljon.
     def return_smallest_number(self):
         smallest_number = 1000000
         for item in self.list:
@@ -36,6 +40,8 @@ class ELf_list():
                 smallest_number = item
         return smallest_number
 
+    # Input är ett värde som ska ersätta minsta talet. Ersätter minsta talet med detta. Kollar INTE att värdet är mindre.
+    # Detta måste man göra utanför programmet. Denna kommer bara ersätta minsta talet i listan med value.
     def substitute_smallest_number(self, value):
         smallest_value = self.return_smallest_number()
         for index in range(len(self.list)):
@@ -43,27 +49,29 @@ class ELf_list():
                 self.list[index] = value
                 break
 
+    # Printar summan av de tre värdena.
     def print_sum(self):
         print (self.list[0] + self.list[1] + self.list[2])
 
 
 file = open("Day1_input.txt", 'r')
-high_calorie_elfs = ELf_list()
+high_calorie_elfs = ELf_list()  # Skapar ett objekt
 current_calorie = 0 # Håller koll på nuvarande elfs kalorimängd
+
 
 for row in file:
     if row == "\n": # Hit kommer vi när vi byter elf, dvs vid tom rad
 
-        if current_calorie > high_calorie_elfs.return_smallest_number():   # Om det är nytt rekord mmärker vi det som nytt highest
-            high_calorie_elfs.substitute_smallest_number(current_calorie)
-            current_calorie = 0 # Nollställer
+        if current_calorie > high_calorie_elfs.return_smallest_number():   # Kollar om värdet är större än tredjehögsta
+            high_calorie_elfs.substitute_smallest_number(current_calorie)   # Ersätter med lägsta värdet
+            current_calorie = 0 # Nollställer vad nuvarande elf har
 
         else:
             current_calorie = 0 # Nollställer nuvarande elf räkningen
 
     else:   # När vi inte har kommit till en ny elf adderar vi fortsatt alla kalorier
         row.strip()
-        current_calorie += int(row) # Adderar till listan
+        current_calorie += int(row) # Adderar värdet till den nuvarande elfens totala kaloriintag
 
 high_calorie_elfs.print_sum()
 file.close()
